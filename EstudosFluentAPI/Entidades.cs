b61218afdc69a266be16f6b265901d4297a385f2
Jonathan Paiva <jonathan.paiva.m@hotmail.com>
2021-08-04 19:02:37 -0300

    Atualização de Propriedades de Fluent API

--- EstudosFluentAPI/Entidades.cs
+++ EstudosFluentAPI/Entidades.cs
@@ -11,7 +11,8 @@ namespace EstudosFluentAPI
 
         public class Produto
         {
-            public int ID { get; set; }
+            public int CodigoProduto { get; set; }
+            public string Referencia { get; set; }
             public string NomeProduto { get; set; }
             public double PrecoProduto { get; set; }
         }
@@ -21,6 +22,13 @@ namespace EstudosFluentAPI
             public int ID { get; set; }
             public string NomeGrupo { get; set; }
         }
+
+        public class Cliente
+        {
+            public int Codigo { get; set; }
+            public string Nome { get; set; }
+        }
+
 
         public class Fabricante
         {
--- EstudosFluentAPI/EntidadesContext.cs
+++ EstudosFluentAPI/EntidadesContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace EstudosFluentAPI
 {
@@ -10,6 +11,8 @@ namespace EstudosFluentAPI
 
         public DbSet<Entidades.Fabricante> Fabricantes { get; set; }
 
+        public DbSet<Entidades.Cliente  > Clientes { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source = localhost\\sqlexpress; Initial Catalog = " +
@@ -19,8 +22,28 @@ namespace EstudosFluentAPI
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //Configurações sobre Fluent API
-            modelBuilder.HasDefaultSchema("Admin");
-            modelBuilder.Ignore<Entidades.Fabricante>();
+
+            /*modelBuilder.Entity(typeof("Nome da Entidade"))... // Configuração não genérica */
+            /*modelBuilder.Entity("Nome da Entidade")... // Configuração não genérica */
+            /*modelBuilder.Entity<Nome da Entidade>()... // Configuração genérica */
+
+            modelBuilder.HasDefaultSchema("Admin"); //Define o Schema utilizado nas tabelas do Banco de Dados
+            modelBuilder.Ignore<Entidades.Fabricante>();  //Define se a Tabela vai ser encaminhado para a migration para ser criada no BD
+
+            modelBuilder.Entity<Entidades.Cliente>()
+                .ToTable("Pessoas") // Define o nome para a Entidade(Tabela) no BD
+                .HasKey(t => t.Codigo); // Define qual será Primary Key da tabela
+
+
+            modelBuilder.Entity<Entidades.Produto>()
+                .HasKey(p => new { p.CodigoProduto, p.Referencia }); //Criar uma chave composta na tabela
+
+            modelBuilder.Entity<Entidades.Grupo>()
+                .HasIndex(g => g.NomeGrupo).IsUnique(); // HasIndex cria um índice para a columa / IsUnique cria um índice único para a coluna
+
+            modelBuilder.Entity<Entidades.Grupo>()
+                .HasAlternateKey(g => g.NomeGrupo); // Criar uma restrição exclusiva diferentes daquelas que foram a chave primária.
+
         }
     }
 }
