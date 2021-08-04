b1a57a51019f0750a8c84f54e420b528d328bf0c
Jonathan Paiva <jonathan.paiva.m@hotmail.com>
2021-08-04 18:25:33 -0300

    Comite inicial - Estudos Fluent API

---
+++ EstudosFluentAPI/Entidades.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudosFluentAPI
+{
+    class Entidades
+    {
+
+        public class Produto
+        {
+            public int ID { get; set; }
+            public string NomeProduto { get; set; }
+            public double PrecoProduto { get; set; }
+        }
+            
+        public class Grupo
+        {
+            public int ID { get; set; }
+            public string NomeGrupo { get; set; }
+        }
+
+        public class Fabricante
+        {
+            public int ID { get; set; }
+            public string NomeFabricante { get; set; }
+        }
+    }
+}
---
+++ EstudosFluentAPI/EntidadesContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EstudosFluentAPI
+{
+    class EntidadesContext: DbContext
+    {
+        public DbSet<Entidades.Produto> Produtos { get; set; }
+
+        public DbSet<Entidades.Grupo> Grupos { get; set; }
+
+        public DbSet<Entidades.Fabricante> Fabricantes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = localhost\\sqlexpress; Initial Catalog = " +
+                                        "EstudosDBFluentAPI;Persist Security Info=True;User ID=sa;Password=qaz@123");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Configurações sobre Fluent API
+            modelBuilder.HasDefaultSchema("Admin");
+            modelBuilder.Ignore<Entidades.Fabricante>();
+        }
+    }
+}
